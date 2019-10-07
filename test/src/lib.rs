4e061d93039c2a825824f49d1846f5571178c274
Yurihaia <17830663+Yurihaia@users.noreply.github.com>
2019-10-07 16:16:17 -0400

    Add *better* error messages for testing

--- test/src/lib.rs
+++ test/src/lib.rs
@@ -1,3 +1,5 @@
+#![type_length_limit="7000000"]
+
 #[cfg(test)]
 mod tests {
 	const TARGET: &str = "../minecraft";
@@ -6,44 +8,32 @@ mod tests {
 	use glob::glob;
 	use std::fs;
 
-	// This is a very sketchy function, but it actually works
-	fn str_before<'a>(src: &'a str, target: &'a str) -> Option<&'a str> {
-		if target.as_ptr() < src.as_ptr() {
-			None
-		} else {
-			let offset = target.as_ptr() as usize - src.as_ptr() as usize;
-			src.get(0..offset)
-		}
-	}
-
 	#[test]
 	fn check_syntax() {
 		let paths = glob("../minecraft/**/*.nbtdoc").unwrap();
-		let mut fail = vec![];
+		let mut panic = false;
 		for path in paths {
 			let path = path.unwrap();
 			let ress = fs::read_to_string(&path).unwrap();
-			let res = nbtdoc::parse::root(&ress);
+			let res = nbtdoc::parse::root::<nom::error::VerboseError<&str>>(&ress);
 			match res {
-				Ok((v, _)) => assert_eq!(v, ""),
-				Err(nom::Err::Error((r, e))) | Err(nom::Err::Failure((r, e))) => {
-					let offset = str_before(ress.as_str(), r).unwrap().trim_end();
-					let line = offset.split('\n').count();
-					let col = offset.split('\n').next_back().unwrap().len();
-					let pathstr = path.to_str().unwrap();
-					eprintln!(
-						"-   Error {} in file {} at {}:{}",
-						e.description(),
-						pathstr,
-						line,
-						col
-					);
-					fail.push(String::from(pathstr));
-				},
-				_ => panic!()
+				Ok(_) => (),
+				Err(e) => match e {
+					nom::Err::Error(e) | nom::Err::Failure(e) => {
+						eprintln!(
+							"Error at file {}: {}",
+							path.to_str().unwrap(),
+							nom::error::convert_error(&ress, e)
+						);
+						panic = true;
+					},
+					_ => panic!()
+				}
 			}
 		}
-		panic!()
+		if panic {
+			panic!()
+		}
 	}
 
 	#[test]
