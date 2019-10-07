6a2a6797122666b4220e886d598a883f4fda7b6a
Yurihaia <17830663+Yurihaia@users.noreply.github.com>
2019-10-07 11:36:36 -0400

    improve test error messages

--- test/src/lib.rs
+++ test/src/lib.rs
@@ -6,14 +6,44 @@ mod tests {
 	use glob::glob;
 	use std::fs;
 
+	// This is a very sketchy function, but it actually works
+	fn str_before<'a>(src: &'a str, target: &'a str) -> Option<&'a str> {
+		if target.as_ptr() < src.as_ptr() {
+			None
+		} else {
+			let offset = target.as_ptr() as usize - src.as_ptr() as usize;
+			src.get(0..offset)
+		}
+	}
+
 	#[test]
 	fn check_syntax() {
 		let paths = glob("../minecraft/**/*.nbtdoc").unwrap();
+		let mut fail = vec![];
 		for path in paths {
-			let res = fs::read_to_string(path.unwrap()).unwrap();
-			let res = nbtdoc::parse::root(&res).unwrap();
-			assert_eq!(res.0.trim(), "");
+			let path = path.unwrap();
+			let ress = fs::read_to_string(&path).unwrap();
+			let res = nbtdoc::parse::root(&ress);
+			match res {
+				Ok((v, _)) => assert_eq!(v, ""),
+				Err(nom::Err::Error((r, e))) | Err(nom::Err::Failure((r, e))) => {
+					let offset = str_before(ress.as_str(), r).unwrap().trim_end();
+					let line = offset.split('\n').count();
+					let col = offset.split('\n').next_back().unwrap().len();
+					let pathstr = path.to_str().unwrap();
+					eprintln!(
+						"-   Error {} in file {} at {}:{}",
+						e.description(),
+						pathstr,
+						line,
+						col
+					);
+					fail.push(String::from(pathstr));
+				},
+				_ => panic!()
+			}
 		}
+		panic!()
 	}
 
 	#[test]
