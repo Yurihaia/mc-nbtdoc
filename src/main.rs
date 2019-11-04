9e705d7ee2e61ce2fba8ed95d650dc1861abe977
MrYurihi <mryurihi2003@gmail.com>
2019-11-04 14:38:27 -0500

    create build branch and pull in at 19w40a

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![type_length_limit="60000000"]
+
+const SOURCE: &str = "./mc-nbtdoc/minecraft/.";
+const DEST: &str = "./build/.";
+
+use std::fs::File;
+use std::path::Path;
+
+use serde_json::ser::PrettyFormatter;
+
+use serde::Serialize;
+
+fn main() {
+	let mut root = nbtdoc::Root::new();
+	root.add_root_module(SOURCE, &nbtdoc::DefaultFileProvider).unwrap();
+	let dest = Path::new(DEST);
+	serde_json::to_writer(
+		File::create(dest.join("generated.json")).unwrap(),
+		&root
+	).unwrap();
+	root.serialize(&mut serde_json::Serializer::with_formatter(
+		File::create(dest.join("generated.pretty.json")).unwrap(),
+		PrettyFormatter::with_indent(b"\t")
+	)).unwrap();
+}
